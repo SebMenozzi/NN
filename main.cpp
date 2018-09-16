eef022085e86af3db64c0c7bc20a2698034cbf08
SebMenozzi <seb.menozzi@hotmail.fr>
2018-09-16 21:53:41 +0200

    WIP

--- main.cpp
+++ main.cpp
@@ -1,43 +1,151 @@
 #include <iostream>
+#include <math.h>
 #include "matrix.h"
 
-int main()
-{
-    Matrix a(3, 2);
-    Matrix b(2, 4);
-    try
-    {
-  		a.setValue(0, 0, 6.0);
-  		a.setValue(0, 1, -1);
+#define nb_neurons_hidden_layer 3
+#define epoch 1
+#define learning_rate 0.8
 
-  		a.setValue(1, 0, -4);
-  		a.setValue(1, 1, 0);
+// GLOBAL
+const int training_data[4][2] = {
+    { 1, 0 },
+    { 1, 1 },
+    { 0, 1 },
+    { 0, 0 }
+};
+const int anwser_data[4] = { 1, 0, 1, 0 };
 
-  		a.setValue(2, 0, 1);
-  		a.setValue(2, 1, -3);
+int target;
 
-  		b.setValue(0, 0, -2.0);
-  		b.setValue(0, 1, 2.0);
-      b.setValue(0, 2, -1.0);
-      b.setValue(0, 3, 3.0);
+Matrix input_to_hidden_weights(nb_neurons_hidden_layer, 2);
+Matrix input_to_hidden_bias(2, 1);
+Matrix input_layer(2, 1);
 
-      b.setValue(1, 0, 4.0);
-  		b.setValue(1, 1, 0);
-      b.setValue(1, 2, 5.0);
-      b.setValue(1, 3, 6.0);
+Matrix hidden_to_output_weights(1, nb_neurons_hidden_layer);
+Matrix hidden_to_output_bias(nb_neurons_hidden_layer, 1);
+Matrix hidden_layer(nb_neurons_hidden_layer, 1);
 
-    	a.display();
-    	std::cout << std::endl;
+Matrix output_layer(1, 1);
 
-    	b.display();
-    	std::cout << std::endl;
+float error_rate; // for debug
 
-    	Matrix c = a * b;
-    	c.display();
+// UTILS
+
+double random_between_neg_1_pos_1() {
+    int randNumber = rand() % 2; // 0 or 1
+    double result = rand() / (RAND_MAX + 1.0); // between 0 and 1
+    return (randNumber == 1) ? result : -1 * result;
+}
+
+void generate_random_input_to_hidden_weights() {
+    for (size_t row = 0; row < nb_neurons_hidden_layer; row++) {
+        for (size_t column = 0; column < 2; column++)
+        {
+            input_to_hidden_weights.setValue(row, column, random_between_neg_1_pos_1());
+        }
     }
-    catch(std::string error)
+}
+
+void generate_random_hidden_to_output_weights() {
+    for (size_t row = 0; row < nb_neurons_hidden_layer; row++)
     {
-    	std::cout << error << std::endl;
+        hidden_to_output_weights.setValue(0, row, random_between_neg_1_pos_1());
+    }
+}
+
+// activation function
+float sigmoid(float x)
+{
+    return 1.0 / (1.0 + exp(-x));
+}
+
+// derivative of activation function
+float sigmoid_prime(float x)
+{
+    return x * (1.0 - x);
+}
+
+Matrix applySigmoid(Matrix a) {
+  for (size_t column = 0; column < a.getHeight(); column++) {
+    for (size_t row = 0; row < a.getWidth(); row++) {
+      a.setValue(column, row, sigmoid(a.getValue(column, row)));
+    }
+  }
+  return a;
+}
+
+// FORWARD PROPAGATION
+
+void forward_propagate() {
+    hidden_layer = applySigmoid(input_to_hidden_weights * input_layer + input_to_hidden_bias);
+    output_layer = applySigmoid(hidden_to_output_weights * hidden_layer + hidden_to_output_bias);
+}
+
+/*
+// BACKWARD PROPAGATION
+void back_propagate() {
+    float error = output_layer - target;
+    error_rate = pow(error, 2);
+
+    for (int i = 0; i < nb_neurons_hidden_layer; i++) {
+        for (int j = 0; j < 2; j++) {
+            float deltaw1 = error *
+                            sigmoid_prime(output_layer) *
+                            hidden_to_output_weights[i] *
+                            sigmoid_prime(hidden_layer[i]) *
+                            input_layer[j];
+            // new input weights
+            input_to_hidden_weights[i][j] -= deltaw1 * learning_rate;
+        }
+
+        float deltaw2 = error *
+                        sigmoid_prime(output_layer) *
+                        hidden_layer[i];
+        // new hidden weights
+        hidden_to_output_weights[i] -= deltaw2 * learning_rate;
+
+        float deltab1 = error *
+                        sigmoid_prime(output_layer) *
+                        hidden_to_output_weights[i] *
+                        sigmoid_prime(hidden_layer[i]);
+        // new input bias
+        input_to_hidden_bias[i] -= deltab1 * learning_rate;
+
+        float deltab2 = error *
+                        sigmoid_prime(output_layer);
+        // new hidden bias
+        hidden_to_output_bias -= deltab2 * learning_rate;
+    }
+}
+*/
+void display_result() {
+    input_layer.display();
+
+    std::cout << "(" << input_layer.getValue(0, 0) << ", " << input_layer.getValue(1, 0) << ") : expected: " << target << " (error:" << output_layer.getValue(0, 0) << ")" << std::endl << std::endl;
+}
+int main() {
+    generate_random_input_to_hidden_weights();
+    generate_random_hidden_to_output_weights();
+
+    /*
+    input_to_hidden_weights.display();
+
+    std::cout << std::endl << "-----------" << std::endl << std::endl;
+
+    hidden_to_output_weights.display();
+    */
+
+    // train the network
+    for (int i = 0; i < epoch; i++) {
+        for (int inputs = 0; inputs < 4; inputs++) {
+            input_layer.setValue(0, 0, training_data[inputs][0]);
+            input_layer.setValue(1, 0, training_data[inputs][1]);
+            target = anwser_data[inputs];
+
+            forward_propagate();
+            //back_propagate();
+            //display_result();
+        }
     }
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -11,34 +11,40 @@ class Matrix
 		std::vector<std::vector<float> > values;
     bool equal(const Matrix& matrix) const;
 		void resize(size_t newWidth, size_t newHeight);
-
-	public:
-		Matrix();
-		Matrix(size_t height, size_t width);
-		Matrix(const Matrix& matrixToCopy);
-		~Matrix();
-		void display();
 		void add(const Matrix& matrix);
 		void add(float n);
+		void subtract(const Matrix& matrix);
 		void subtract(float n);
 		void multiply(const Matrix& matrix);
 		void multiply(float n);
-		void divide(const Matrix& matrix);
+
+	public:
+		Matrix(); // checked
+		Matrix(size_t height, size_t width); // checked
+		Matrix(const Matrix& matrixToCopy); // checked
+		void display(); // checked
 		void divide(float n);
-		size_t getWidth(void) const;
-		size_t getHeight(void) const;
+		size_t getWidth(void) const; // checked
+		size_t getHeight(void) const; // checked
 		void setValue(size_t line, size_t column, float value);
 		float getValue(size_t line, size_t column) const;
 		std::vector<std::vector<float> > getValues(void) const;
 		std::vector<float> operator[] (size_t x) const;
-		Matrix operator+(const Matrix& matrix) const;
-		Matrix operator+(float n) const;
-		Matrix operator-(float n) const;
-		Matrix operator*(const Matrix& matrix) const;
-		Matrix operator*(float n) const;
+		Matrix operator+(const Matrix& matrix) const; // checked
+		Matrix operator+(float n) const; // checked
+		Matrix operator-(const Matrix& matrix) const; // checked
+		Matrix operator-(float n) const; // checked
+		Matrix operator*(const Matrix& matrix) const; // checked
+		Matrix operator*(float n) const; // checked
 		Matrix operator/(float n) const;
-    bool operator==(const Matrix& matrix) const;
-    bool operator!=(const Matrix& matrix) const;
+    bool operator==(const Matrix& matrix) const; // checked
+    bool operator!=(const Matrix& matrix) const; // checked
+    Matrix& operator+=(const Matrix& matrix); // checked
+    Matrix& operator+=(float n); // checked
+    Matrix& operator-=(const Matrix& matrix); // checked
+    Matrix& operator-=(float n); // checked
+    Matrix& operator*=(const Matrix& matrix); // checked
+    Matrix& operator*=(float n); // checked
 };
 
 #endif // MATRIX_H_INCLUDED
